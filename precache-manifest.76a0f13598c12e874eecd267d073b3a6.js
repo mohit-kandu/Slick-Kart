f98748d2e8edc9020b0d7f98c7e40f54cb9cc929
mohit-kandu <once.mohit@gmail.com>
2021-08-16 21:27:38 +0545

    Updates

---
+++ precache-manifest.76a0f13598c12e874eecd267d073b3a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b04b0443c138daa612805aa529162d",
+    "url": "/Flipkart-Clone/index.html"
+  },
+  {
+    "revision": "c29a095ef1910cc3a790",
+    "url": "/Flipkart-Clone/static/css/main.67c4475b.chunk.css"
+  },
+  {
+    "revision": "7b0bb173f6ce6cf9297b",
+    "url": "/Flipkart-Clone/static/js/2.e7e93a0c.chunk.js"
+  },
+  {
+    "revision": "a1f0948bbd8d9ece1696d016e9ec21db",
+    "url": "/Flipkart-Clone/static/js/2.e7e93a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29a095ef1910cc3a790",
+    "url": "/Flipkart-Clone/static/js/main.37cb98a8.chunk.js"
+  },
+  {
+    "revision": "cc5a5fb8ec75b5bbbbde",
+    "url": "/Flipkart-Clone/static/js/runtime-main.4915fbe3.js"
+  }
+]);
